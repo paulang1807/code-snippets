ba3fb113c8be54f2be1c5cd772711428852b82b9
Angshuman Paul <paulang1807@gmail.com>
2020-11-24 11:42:13 -0800

    sample pyspark code for create dataframes manually

---
+++ pyspark/createSampleDataFrame.py
@@ -0,0 +1,17 @@
+""" 
+Code snippet to create a pyspark dataframe manually
+and convert a date column from string to date data type
+"""
+
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType, TimestampType
+from pyspark.sql.functions import to_timestamp
+
+schema = StructType([StructField("date", StringType(), True), StructField("product", StringType(), True), StructField("channel", StringType(), True), StructField("quantity", IntegerType(), True)])
+
+df = spark.createDataFrame([("09-10-2016","p1","c1", 1),("09-10-2016","p2","c1", 1),("09-11-2016","p1","c2", 2),("09-14-2016","p2","c2", 0),("09-16-2016","p1","c2", 1),("09-17-2016","p2","c1", 0),("09-20-2016","p1","c2", 2)],schema)
+
+df.printSchema()
+
+df2 = df.select(to_timestamp(df.date, 'MM-dd-yyyy').alias('dt'),df.product, df.channel, df.quantity)
+
+df2.printSchema()
